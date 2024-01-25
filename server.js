feaf17e07009b3e37e9dac835ea3da6f0f9cc664
goatcheese82 <smi02045@byui.edu>
2024-01-25 16:24:08 -0500

    w03 learning activities

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 const expressLayouts = require("express-ejs-layouts")
-
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -26,9 +27,9 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index Route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
