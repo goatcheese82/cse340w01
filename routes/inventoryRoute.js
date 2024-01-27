80563dff4cb86e78bf20d188d3dcb7597e15826e
goatcheese82 <smi02045@byui.edu>
2024-01-27 11:55:20 -0500

    W03 Assignment: Vehicle Detail Page-Errors

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,10 +1,11 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
-const invController = require("../controllers/invController")
+const invController = require("../controllers/invController");
+const { handleErrors } = require("../utilities");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/detail/:inventoryId", invController.buildByInventoryId);
+router.get("/type/:classificationId", handleErrors(invController.buildByClassificationId));
+router.get("/detail/:inventoryId", handleErrors(invController.buildByInventoryId));
 
 module.exports = router;
