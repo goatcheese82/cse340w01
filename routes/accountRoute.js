0805e4a1765237f9c6a407d8310c667fe1ef4167
goatcheese82 <smi02045@byui.edu>
2024-02-03 16:55:19 -0500

    w04 server val

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,13 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation');
 
 // Route to deliver login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to deliver register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 // Route to post registration information to db
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post("/register", regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount));
 
 module.exports = router;
